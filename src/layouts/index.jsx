fb39f04ee872ac39229f080b0b5e7ca1d09f3a2e
DavidBrito32 <davidbrito.carneiro458@gmail.com>
2023-12-02 16:24:06 -0300

    adicionando a extensao do arquivo header

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom/";
-import Header from "../components/header/";
+import Header from "../components/header/index.jsx";
 import { styled } from "styled-components/";
 import Footer from "../components/footer/";
 

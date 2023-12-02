3d33e85e23ea2f2a5c95f5e09280bc5bc9de247c
DavidBrito32 <davidbrito.carneiro458@gmail.com>
2023-12-02 16:20:55 -0300

    Correçao do header

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,7 +1,7 @@
-import { Outlet } from "react-router-dom";
-import Header from "../components/header";
-import { styled } from "styled-components";
-import Footer from "../components/footer";
+import { Outlet } from "react-router-dom/";
+import Header from "../components/header/";
+import { styled } from "styled-components/";
+import Footer from "../components/footer/";
 
 const Publiclayout = () => {
   return (
